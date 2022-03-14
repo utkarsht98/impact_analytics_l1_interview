34256ebf0412b97edef23640a7d117760b075ae9
UTKARSH TIWARI <utkarshtiwari2803@gmail.com>
2022-03-14 15:44:42 +0530

    Return statement
    Return buy Index and sell Index along with max profit

--- test.py
+++ test.py
@@ -4,18 +4,24 @@
 # [10,2,8,7,1,2,1]
 import math
 
-def stockPrice(price):
+def maxStockProfit(price):
     currBought = price[0]
     maxProfit = -math.inf
     buyIndex = 0
     sellIndex = -1
-    for i in range(1, price):
+    currBuyIndex = 0
+    # currBuyIndex = 0
+    for i in range(1, len(price)):
         if price[i] < currBought:
             currBought = price[i]
-            buyIndex = i
+            currBuyIndex = i
         else:
             diff = price[i]-currBought
             if maxProfit < diff:
+                buyIndex = currBuyIndex
                 sellIndex = i
                 maxProfit = diff
-    return maxProfit
+    return maxProfit, buyIndex, sellIndex
+
+stockPrice = [ 10, 1, 8, 6, 2]
+print(maxStockProfit(stockPrice))
